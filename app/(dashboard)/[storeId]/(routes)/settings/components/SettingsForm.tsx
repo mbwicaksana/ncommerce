e7507e88629a4f1be11a6a19a0cfd42bcc5f11a6
Muhammad Bayu Wicaksana <mbwicaksana.98@gmail.com>
2024-07-25 11:04:00 +0700

    Office 04:30:00

--- app/(dashboard)/[storeId]/(routes)/settings/components/SettingsForm.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/SettingsForm.tsx
@@ -14,6 +14,7 @@ import {
 import { Heading } from "@/components/ui/heading";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
+import { useOrigin } from "@/hooks/use-origin";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Store } from "@prisma/client";
 import axios from "axios";
@@ -37,6 +38,8 @@ type SettingsFormValues = z.infer<typeof formSchema>;
 const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
   const params = useParams();
   const router = useRouter();
+  const origin = useOrigin();
+
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
 
@@ -123,7 +126,11 @@ const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
         </form>
       </Form>
       <Separator />
-      <ApiAlert title="test title" description="test description" />
+      <ApiAlert
+        title="PUBLIC_API_URL"
+        description={`${origin}/api/${params.storeId}`}
+        variant="public"
+      />
     </>
   );
 };
